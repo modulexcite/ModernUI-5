28f18fe764f9a29e0391c00f78143942d25f5f05
maigerone@gmail.com <maigerone@gmail.com>
2013-03-13 22:07:05 +0800

    fit commit

---
+++ ModernUI/Controls/ModernWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ModernUI.Controls
+{
+    public class ModernWindow : Window
+    {
+        public static readonly DependencyProperty TitleBarHeightProperty =
+            DependencyProperty.Register("TitleBarHeight", typeof(int), typeof(ModernWindow), new PropertyMetadata(36));
+        
+        public static readonly DependencyProperty TitleBarForgroundProperty =
+           DependencyProperty.Register("TitleBarForground", typeof(Brush), typeof(ModernWindow));
+
+        
+
+        public ModernWindow()
+        {
+            //DefaultStyleKey = typeof(ModernWindow);
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ModernWindow), new FrameworkPropertyMetadata(typeof(ModernWindow)));
+        }
+
+
+
+        public Brush TitleBarForground
+        {
+            get { return (Brush)GetValue(TitleBarForgroundProperty); }
+            set { SetValue(TitleBarForgroundProperty, value); }
+        }
+
+        public int TitleBarHeight
+        {
+            get { return (int)GetValue(TitleBarHeightProperty); }
+            set { SetValue(TitleBarHeightProperty, value); }
+        }
+    }
+}
